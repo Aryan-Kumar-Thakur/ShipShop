4b1ce51333d4e6e796a421f770560a03ef7f4828
Aryan-Kumar-Thakur <thakuraryan7678@gmail.com>
2024-06-14 03:05:36 +0530

    Minor Fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		outDir: "build"
+	},
 	server: {
 		port: 3000,
-    open: true,
+		open: true,
 		proxy: {
 			"/api": {
 				target: "http://localhost:8000",
