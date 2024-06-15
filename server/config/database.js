2fcd8f91f26baffdccab48707718f3dd33e2b21c
Aryan-Kumar-Thakur <thakuraryan7678@gmail.com>
2024-06-16 00:30:22 +0530

    Minor Fix

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDatabase = ()=>{
     mongoose.connect(process.env.DB_URI,{
-        dbName: "Ecommerce"
+        dbName: "ShipShop"
     }).then((data)=>{
         console.log(`mondodb is connected with server: ${data.connection.host}`)
     })
